fa8c818d403b9d4ce974d9e1eadd3cafc2a93afa
watisagoodusername <144091194+watisagoodusername@users.noreply.github.com>
2024-05-27 22:01:01 +1000

    Delete FinalProject directory

--- FinalProject/Booking/admin.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
- 
-from django.contrib import admin
-from .models import Scheduling
- 
-class EventAdmin(admin.ModelAdmin):
-    list_display = ['day', 'timebooked', 'returntime', 'notes']
